f4e7cb76dd6ca162f062fb8bdf2b102575979cb6
Exortix <40566204+Exortix@users.noreply.github.com>
2022-02-20 17:54:24 +0300

    New Look

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 BASE_URL = "https://gogoplay1.com"
 
+let searchParams = new URLSearchParams(window.location.search);
+let pageNumber = searchParams.get('page');
+
+
+$(document).ready(() => {
+    let searchParams = new URLSearchParams(window.location.search);
+        if (!searchParams.has('page'))
+            window.location.href = `${window.location.href.split('?')[0]}?page=1`;
+        let pageNumber = searchParams.get('page');;
+        let searchText = $('#searchText').val();
+        getAnimes(searchText)
+});
 
 
 
@@ -22,7 +34,7 @@ function getAnimes(query) {
     var url;
 
     if (searchText.length < 2)
-        url = `${BASE_URL}`;
+        url = `${BASE_URL}/?page=${pageNumber}`;
     else
         url = `${BASE_URL}/search.html?keyword=${query}`
 
@@ -34,6 +46,7 @@ function getAnimes(query) {
           var parser = new DOMParser();
           var doc = parser.parseFromString(xhr.responseText, "text/html");
           var animes = doc.querySelectorAll('ul.listing.items > li.video-block > a');
+          var pagination = doc.querySelector("#main_bg > div:nth-child(5) > div > div.vc_row.wpb_row.vc_row-fluid.vc_custom_1404913114846 > div:nth-child(3)")
 
           let output = '';
           $.each(animes, (index, anime) => {
@@ -45,11 +58,12 @@ function getAnimes(query) {
                 <div onclick="window.location.href = 'anime/?id=${id}&ep=${ep}'" class="col anime rounded">
                     <img class = "rounded" src = "${anime.querySelector('img').src}"/>
                     <h4>${anime.querySelector('div.name').innerText}</h4>
-                    <p class="anime-overview">${(anime.querySelector('meta') != null) ? anime.querySelector('meta').innerText : ''}</p>
+                    <span class="anime-overview">${(anime.querySelector('.meta') != null) ? anime.querySelector('.meta').innerText : ''}</span>
                 </div>
             `;
           });
           $('#animes > .row').html(output);
+          $('#pagination').html(pagination);
        }};
     
     xhr.send();
